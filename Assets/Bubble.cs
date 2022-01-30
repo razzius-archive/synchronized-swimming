78052bed6f61e4579a80e2c02231e5dd1782462d
Razzi Abuissa <razzi@abuissa.net>
2022-01-30 15:25:37 -0600

    ignore fish collision with inner bubble

--- Assets/Bubble.cs
+++ Assets/Bubble.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class Bubble : MonoBehaviour
 {
+    private Rigidbody2D body;
     // Start is called before the first frame update
     void Start()
     {
-        
+        body = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    
+    void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.name == "PrizeFish1") {
+            collision.gameObject.transform.position = transform.position;
+        }
     }
 }
---
+++ Assets/InnerCircle.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InnerCircle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+        foreach (GameObject player in players) {
+        Debug.Log(player.name);
+            Physics2D.IgnoreCollision(player.GetComponent<CapsuleCollider2D>(), GetComponent<Collider2D>());
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
