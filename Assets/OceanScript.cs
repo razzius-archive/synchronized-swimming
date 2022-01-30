bcc9f013b20208cd1ceac66dc621932ab62b0a4a
Razzi Abuissa <razzi@abuissa.net>
2022-01-29 23:39:01 -0600

    Sound when you hit the ocean

--- Assets/OceanScript.cs
+++ Assets/OceanScript.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class OceanScript : MonoBehaviour
 {
+    public AudioSource source;
     // Start is called before the first frame update
     void Start()
     {
-        
+        source = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -19,6 +20,7 @@ public class OceanScript : MonoBehaviour
     void OnCollisionEnter2D(Collision2D collision) {
     	Debug.Log(collision.gameObject.name);
         if (collision.gameObject.name == "Circle") {
+            source.Play();
             Destroy(collision.gameObject);
         }
     }
